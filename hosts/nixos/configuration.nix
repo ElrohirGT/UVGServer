21ea4ee828f8c687d4e9610914160d6ca5e380d6
ElrohirGT <elrohirgt@gmail.com>
2024-09-17 18:50:38 -0600

    fix: vim reference

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -29,7 +29,7 @@
     isNormalUser = true;
     extraGroups = ["wheel"];
     packages = let
-      vimRice = self.inputs.flaviosConfiguration.outputs.vim;
+      vimRice = self.inputs.flaviosConfiguration.outputs.packages.x86_64-linux.vim;
     in [vimRice];
   };
 
